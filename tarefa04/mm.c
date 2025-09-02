4d894f5f4c67543c90993ab5779c9181434daa1f
Catarina F. M. Castro <catfmcastro@gmail.com>
2025-09-02 09:15:32 -0300

    tarefa04

---
+++ tarefa04/mm.c
@@ -0,0 +1,51 @@
+//  tempo sequencial: 1m13.294s
+//  tempo paralelo: 0m19.022s
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void mm(double *a, double *b, double *c, int width) {
+
+#pragma omp parallel for
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < width; j++) {
+      double sum = 0;
+      for (int k = 0; k < width; k++) {
+        double x = a[i * width + k];
+        double y = b[k * width + j];
+        sum += x * y;
+      }
+      c[i * width + j] = sum;
+    }
+  }
+}
+
+int main() {
+  int width = 2000;
+  double *a = (double *)malloc(width * width * sizeof(double));
+  double *b = (double *)malloc(width * width * sizeof(double));
+  double *c = (double *)malloc(width * width * sizeof(double));
+
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < width; j++) {
+      a[i * width + j] = i;
+      b[i * width + j] = j;
+      c[i * width + j] = 0;
+    }
+  }
+
+  mm(a, b, c, width);
+
+  free(a);
+  free(b);
+  free(c);
+
+  //  for(int i = 0; i < width; i++) {
+  //  for(int j = 0; j < width; j++) {
+  //    printf("\n c[%d][%d] = %f",i,j,c[i*width+j]);
+  //  }
+  // }
+
+  return 0;
+}
