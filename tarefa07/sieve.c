3b34aa070fbcf781269bcc13c8dc3e1de7df2eac
Catarina F. M. Castro <catfmcastro@gmail.com>
2025-09-07 11:20:59 -0300

    tarefa07

---
+++ tarefa07/sieve.c
@@ -0,0 +1,44 @@
+// tempo sequencial: 0m1.765s
+// tempo paralelo com política estática: 0m1.604s
+// tempo paralelo com política dinâmica: 0m1.664s
+// tempo paralelo com política guiada: 0m1.477s
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sieveOfEratosthenes(int n) {
+  // Create a boolean array "prime[0..n]" and initialize
+  // all entries it as true. A value in prime[i] will
+  // finally be false if i is Not a prime, else true.
+  int primes = 0;
+  bool *prime = (bool *)malloc((n + 1) * sizeof(bool));
+  int sqrt_n = sqrt(n);
+
+  memset(prime, true, (n + 1) * sizeof(bool));
+
+  for (int p = 2; p <= sqrt_n; p++) {
+    // If prime[p] is not changed, then it is a prime
+    if (prime[p] == true) {
+      // Update all multiples of p
+      for (int i = p * 2; i <= n; i += p)
+        prime[i] = false;
+    }
+  }
+
+  // count prime numbers
+#pragma omp parallel for reduction(+ : primes) schedule(guided)
+  for (int p = 2; p <= n; p++)
+    if (prime[p])
+      primes++;
+
+  return (primes);
+}
+
+int main() {
+  int n = 100000000;
+  printf("%d\n", sieveOfEratosthenes(n));
+  return 0;
+}
